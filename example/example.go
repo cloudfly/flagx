05a98d13bfe1cef2960706fe9716a6a25f033ca7
chenyunfei.cs <chenyunfei.cs@bytedance.com>
2025-02-09 14:24:00 +0800

    optmize the public Func's type and docs

--- example/example.go
+++ example/example.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/cloudfly/confo"
+import "github.com/cloudfly/flagx"
 
 type Level string
 
@@ -18,22 +18,22 @@ type Config struct {
 }
 
 var (
-	logFile    = confo.NewString("log.file", "", "the log file")
-	logLevel   = confo.NewBool("log.level", false, "the log level, default is bool")
-	serverHost = confo.NewString("server.addr", ":7070", "the address http will serve on")
-	serverPort = confo.NewInt("server.port", 8080, "the tcp port will listen on")
-	secs       = confo.NewDuration("secs", "30s", "the duration flag, 30 secodns")
-	secs2      = confo.NewDuration("days", "30d", "the duration flag, 30 days")
-	secs3      = confo.NewDuration("weeks", "2w", "the duration flag, 2 weeks")
-	bytes      = confo.NewBytes("bytes", 128, "the bytes flag, 128 Byte")
-	kbs        = confo.NewBytes("kbytes", 64, "the bytes flag, 64KB")
-	mbs        = confo.NewBytes("mbytes", 64, "the bytes flag, 64KB")
-	gbs        = confo.NewBytes("gbytes", 64, "the bytes flag, 64KB")
-	strs       = confo.NewArrayString("array.str", "the string array flag, default is empty")
-	ints       = confo.NewArrayInt("array.int", "the string array flag, default is empty")
+	logFile    = flagx.NewString("log.file", "", "the log file")
+	logLevel   = flagx.NewBool("log.level", false, "the log level, default is bool")
+	serverHost = flagx.NewString("server.addr", ":7070", "the address http will serve on")
+	serverPort = flagx.NewInt("server.port", 8080, "the tcp port will listen on")
+	secs       = flagx.NewDuration("secs", "30s", "the duration flag, 30 secodns")
+	secs2      = flagx.NewDuration("days", "30d", "the duration flag, 30 days")
+	secs3      = flagx.NewDuration("weeks", "2w", "the duration flag, 2 weeks")
+	bytes      = flagx.NewBytes("bytes", 128, "the bytes flag, 128 Byte")
+	kbs        = flagx.NewBytes("kbytes", 64, "the bytes flag, 64KB")
+	mbs        = flagx.NewBytes("mbytes", 64, "the bytes flag, 64KB")
+	gbs        = flagx.NewBytes("gbytes", 64, "the bytes flag, 64KB")
+	strs       = flagx.NewArrayString("array.str", "the string array flag, default is empty")
+	ints       = flagx.NewArrayInt("array.int", "the string array flag, default is empty")
 )
 
 func main() {
-	confo.Parse()
-	confo.Usage("log.file")
+	flagx.Parse()
+	flagx.Usage("log.file")
 }
--- flag.go
+++ flag.go
@@ -10,24 +10,38 @@ import (
 )
 
 var (
-	envPrefix = flag.String("env.prefix", "", "Prefix for environment variables.")
+	envPrefix = flag.String("env.prefix", "", "Name prefix of environment variables that interact with flags.")
 )
 
-var NewBool = flag.Bool
-var NewString = flag.String
-var NewInt = flag.Int
-var NewInt64 = flag.Int64
-var NewFloat = flag.Float64
+// NewBool creates a new bool flag.
+func NewBool(name string, value bool, usage string) *bool {
+	return flag.Bool(name, value, usage+envHelp(name))
+}
+
+// NewString creates a new string flag.
+func NewString(name string, value string, usage string) *string {
+	return flag.String(name, value, usage+envHelp(name))
+}
+
+// NewInt creates a new int flag.
+func NewInt(name string, value int, usage string) *int {
+	return flag.Int(name, value, usage+envHelp(name))
+}
+
+// NewInt64 creates a new int64 flag.
+func NewInt64(name string, value int64, usage string) *int64 {
+	return flag.Int64(name, value, usage+envHelp(name))
+}
+
+// NewFloat creates a new float64 flag.
+func NewFloat(name string, value float64, usage string) *float64 {
+	return flag.Float64(name, value, usage+envHelp(name))
+}
 
 // WriteFlags writes all the explicitly set flags to w.
 func WriteFlags(w io.Writer) {
-	flag.Visit(func(f *flag.Flag) {
-		lname := strings.ToLower(f.Name)
-		value := f.Value.String()
-		if IsSecretFlag(lname) {
-			value = "secret"
-		}
-		fmt.Fprintf(w, "-%s=%q\n", f.Name, value)
+	Visit(func(name, value string) {
+		fmt.Fprintf(w, "-%s=%q\n", name, value)
 	})
 }
 
@@ -43,7 +57,7 @@ func Visit(fn func(string, string)) {
 	})
 }
 
-// Parse parses environment vars and command-line flags.
+// Parse parses environment vars(if env.prefix provided) and command-line flags.
 //
 // Flags set via command-line override flags set via environment vars.
 //
@@ -65,21 +79,23 @@ func ParseFlagSet(fs *flag.FlagSet, args []string) {
 		flagsSet[f.Name] = true
 	})
 
-	// Obtain the remaining flag values from environment vars.
-	fs.VisitAll(func(f *flag.Flag) {
-		if flagsSet[f.Name] {
-			// The flag is explicitly set via command-line.
-			return
-		}
-		// Get flag value from environment var.
-		fname := getEnvFlagName(f.Name)
-		if v := os.Getenv(fname); v != "" {
-			if err := fs.Set(f.Name, v); err != nil {
-				// Do not use lib/logger here, since it is uninitialized yet.
-				log.Fatalf("cannot set flag %s to %q, which is read from env var %q: %s", f.Name, v, fname, err)
+	if *envPrefix != "" {
+		// Obtain the remaining flag values from environment vars.
+		fs.VisitAll(func(f *flag.Flag) {
+			if flagsSet[f.Name] {
+				// The flag is explicitly set via command-line.
+				return
 			}
-		}
-	})
+			// Get flag value from environment var.
+			fname := getEnvFlagName(f.Name)
+			if v := os.Getenv(fname); v != "" {
+				if err := fs.Set(f.Name, v); err != nil {
+					// Do not use lib/logger here, since it is uninitialized yet.
+					log.Fatalf("cannot set flag %s to %q, which is read from env var %q: %s", f.Name, v, fname, err)
+				}
+			}
+		})
+	}
 }
 
 func getEnvFlagName(s string) string {
@@ -89,5 +105,8 @@ func getEnvFlagName(s string) string {
 }
 
 func envHelp(s string) string {
+	if *envPrefix == "" {
+		return ""
+	}
 	return fmt.Sprintf("(env: %s)", getEnvFlagName(s))
 }
